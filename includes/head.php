15de99ec52933d75e05ad893d6d30d32549d4fc2
CarlWangShuLin <44082356+CarlWangShuLin@users.noreply.github.com>
2019-04-13 19:59:10 +0800

    carl

---
+++ includes/head.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html >
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!--// Stylesheets //-->
+
+    <link href="../css/login.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <link href="../css/ddsmoothmenu.css" rel="stylesheet" type="text/css" />
+    <link href="../css/scrollbar.css" rel="stylesheet" type="text/css" />
+    <!--// Javascript //-->
+    <script type="text/javascript" src="../js/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/ddsmoothmenu.js"></script>
+    <script type="text/javascript" src="../js/menu.js"></script>
+    <script type="text/javascript" src="../js/contentslider.js"></script>
+    <script type="text/javascript" src="../js/jquery.1.4.2.js"></script>
+    <script type="text/javascript" src="../js/jquery.lint.js"></script>
+    <script type="text/javascript" src="../js/jquery.scroll.js"></script>
+    <script type="text/javascript" src="../js/scroll.js"></script>
+    <script type="text/javascript" src="../js/jquery.idTabs.min.js"></script>
+    <script type="text/javascript" src="../js/switch.js"></script>
+    <script type="text/javascript" src="../js/tabs.js"></script>
+    <script type="text/javascript" src="../js/cufon-yui.js"></script>
+    <script type="text/javascript" src="../js/cufon.js"></script>
+    <script type="text/javascript" src="../js/font.js"></script>
+    <!--[if lte IE 7]><style>.comments .commentlinks{margin-left:250px;}
+.comments .singlebtn{margin-left:380px;}
+.comments ul li:hover .commentlinks{display:inline-block;}
+.comments ul li.level2:hover .commentlinks{margin-left:210px;}
+.comments ul li.level2:hover .singlebtn{margin-left:340px;}
+</style><![endif]--> 
