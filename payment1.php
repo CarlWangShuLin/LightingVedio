43328a845f9af3e81e98e0eeb7b70fd607d629c8
cjm123456 <44083238+cjm123456@users.noreply.github.com>
2019-04-16 15:21:20 +0800

    account
    account

---
+++ payment1.php
@@ -0,0 +1,192 @@
+<html xmlns="http://www.w3.org/1999/xhtml" class="cufon-active cufon-ready"><head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Video 1</title>
+    <!--// Stylesheets //-->
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/ddsmoothmenu.css" rel="stylesheet" type="text/css">
+    <link href="css/scrollbar.css" rel="stylesheet" type="text/css">
+    <!--// Javascript //-->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/ddsmoothmenu.js"></script>
+    <script type="text/javascript" src="js/menu.js"></script>
+    <script type="text/javascript" src="js/contentslider.js"></script>
+    <script type="text/javascript" src="js/jquery.1.4.2.js"></script>
+    <script type="text/javascript" src="js/jquery.lint.js"></script>
+    <script type="text/javascript" src="js/jquery.scroll.js"></script>
+    <script type="text/javascript" src="js/scroll.js"></script>
+    <script type="text/javascript" src="js/jquery.idTabs.min.js"></script>
+    <script type="text/javascript" src="js/switch.js"></script>
+    <script type="text/javascript" src="js/tabs.js"></script>
+    <script type="text/javascript" src="js/cufon-yui.js"></script><style type="text/css">cufon{text-indent:0!important;}@media screen,projection{cufon{display:inline!important;display:inline-block!important;position:relative!important;vertical-align:middle!important;font-size:1px!important;line-height:1px!important;}cufon cufontext{display:-moz-inline-box!important;display:inline-block!important;width:0!important;height:0!important;overflow:hidden!important;text-indent:-10000in!important;}cufon canvas{position:relative!important;}}@media print{cufon{padding:0!important;}cufon canvas{display:none!important;}}</style>
+    <script type="text/javascript" src="js/cufon.js"></script>
+    <script type="text/javascript" src="js/font.js"></script>
+    <!--[if lte IE 7]><style>.comments .commentlinks{margin-left:250px;}
+.comments .singlebtn{margin-left:380px;}
+.comments ul li:hover .commentlinks{display:inline-block;}
+.comments ul li.level2:hover .commentlinks{margin-left:210px;}
+.comments ul li.level2:hover .singlebtn{margin-left:340px;}
+</style><![endif]-->
+</head>
+
+<body class="bg2"><div class="ddshadow toplevelshadow" style="left: 615.271px; top: 89px;"></div>
+    <span class="smalllines">&nbsp;</span>
+    <!-- Wrapper -->
+    <div id="wrapper_sec">
+        <!-- Top Section -->
+        <div class="top_sec">
+            <!-- Top Section Left Links -->
+            <div class="toplinks">
+                <ul>
+                    <li class="first"><a href="index.html">Home</a></li>
+                    <li><a href="login.html">Login</a></li>
+                    <li><a href="signup.html">Signup</a></li>
+                    <li><a href="account.html">My account</a></li>
+                </ul>
+            </div>
+            <!-- Top Section right Links -->
+            <div class="links_icons">
+                <ul>
+                    <li class="last lang">Language: <a href="#"><img src="images/flag1.gif" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <!-- Header -->
+        <div id="masthead">
+            <!-- Logo -->
+            <div class="logo">
+                <a href="index.html"><img src="images/logo.png" style="width:100%;"></a>
+            </div>
+            <!-- Navigation -->
+            <div class="navigation">
+                <div id="smoothmenu1" class="ddsmoothmenu">
+                    <ul>
+                        <li><a href="index.html" class="staticlinks">Home</a></li>
+                        <li><a href="videos.html" class="staticlinks">Videos</a></li>
+                        <li><a href="blogs.html">Blogs</a>
+                        </li>
+                        <li style="z-index: 100;"><a href="ranklist_videos.html" class="staticlinks" style="padding-right: 30px;">Rank lists<img src="images/down.gif" class="downarrowclass" style="border:0;"></a>
+                            <ul style="display: none; top: 30px; visibility: visible;">
+                                <li><a href="ranklist_videos.html">Rank list of videos</a></li>
+                                <li><a href="ranklist_blogs.html">Rank list of blogs</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div class="clear"></div>
+                </div>
+            </div>
+            <!-- Search -->
+            <div class="search">
+                <input type="text" value="Enter keyword to search" id="searchBox" name="s" onblur="if(this.value == '') { this.value = 'Enter keyword to search'; }" onfocus="if(this.value == 'Enter keyword to search') { this.value = ''; }" class="bar">
+                <a href="#" class="go">&nbsp;</a>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <!-- Content Section -->
+        <div id="content_sec">
+            <!-- Bread Crumb -->
+            <div id="crumb">
+               
+                <ul>
+                    <li><a href="profile.html">Profile</a></li>
+                    <li><a href="payment.html">VIP </a></li>
+                    <li><a href="history.html">History </a></li>
+                </ul>
+               
+            </div>
+            <!-- Column 1 -->
+            <div class="col1">
+                <div class="Blog">
+                    <h2 class="heading"><cufon class="cufon cufon-canvas" alt="VIP " style="width: 42px; height: 20px;"><canvas width="57" height="20" style="width: 57px; height: 20px; top: 0px; left: -1px;"></canvas><cufontext>VIP </cufontext></cufon><cufon class="cufon cufon-canvas" alt="Member" style="width: 29px; height: 20px;"><canvas width="41" height="20" style="width: 41px; height: 20px; top: 0px; left: -1px;"></canvas><cufontext>Member</cufontext></cufon></h2>
+                    <ul class="bloglisting">
+                       
+                        <div id="cc_body">
+        <div id="cc_products"><div class="ccp_top_desc" style="font-size: 20px">
+    <span class="ccptd_title">Package selection</span>
+                        </div>
+                        
+                   
+            <div class="nav-item active" style="font-size: 20px">
+        <a class="nav-link" href="payment1.html">
+          <span>Continuous monthly VIP</span>
+              </a>
+      </div>
+            <div class="ccpoil_final_price" style="font-size: 20px">15 yuan</div>
+            
+               
+                       <div class="nav-item active" style="font-size: 20px">
+        <a class="nav-link" href="payment2.html">
+          <span>Annual VIP</span>
+              </a>
+      </div>
+            <div class="ccpoil_final_price" style="font-size: 20px">118 yuan</div>
+                    
+                    
+                </div>
+                
+            </div>
+                        <div class="nav-item active" style="font-size: 20px">
+        <a class="nav-link" href="payment3.html">
+          <span>Season VIP</span>
+              </a>
+      </div>
+            <div class="ccpoil_final_price" style="font-size: 20px">56 yuan</div>
+                        
+                        
+                        <div class="nav-item active" style="font-size: 20px">
+        <a class="nav-link" href="payment4.html">
+          <span>Monthly VIP</span>
+              </a>
+      </div>
+            <div class="ccpoil_final_price" style="font-size: 20px">20 yuan</div>
+                    </ul>
+                    <div class="clear"></div>
+                    <!-- Pagination -->
+                     <div class="cc_pay_title" style="font-size: 20px" data-spm-anchor-id="a2h03.12024492.0.i1">Payment method</div>
+        <div class="cc_pay_ways" style="font-size: 20px">
+            <ul id="ccpw_navs">
+    <li class="ccpw_nav_item current">Sweep code payment</li>
+
+    
+
+</ul>
+            <div id="ccpw_form">
+                <div class="ccpwf_qrcode_bg">
+        
+            <img class="ccpwf_qrcode" src="https://img.alicdn.com/tfs/TB1.t7DGGmWBuNjy1XaXXXCbXXa-128-128.png" style="cursor: pointer" "="" data-spm-anchor-id="a2h03.12024492.0.i2">
+        
+    </div>
+    
+    
+  
+    <div class="ccpwf_price" style="font-size: 20px">
+        <span class="ccpwfp_label">Payment amount:</span>
+        <span class="ccpwfp_final_prize">15</span>
+        <span class="ccpwfp_label">yuan</span>
+        
+    </div>
+    
+
+</div>
+        </div>
+        
+    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <!-- Column 2 -->
+        </div>
+    </div>
+    <div class="clear"></div>
+    <!-- Footer -->
+    <div class="clear"></div>
+    <div id="copyrights">
+        <div class="inner">
+            <p>Copyright © 2019 Group 2. All rights reserved.</p>
+
+        </div>
+    </div>
+
+
+
+</body></html>
---
+++ profile.php
@@ -0,0 +1,198 @@
+<html xmlns="http://www.w3.org/1999/xhtml" class="cufon-active cufon-ready"><head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Video 1</title>
+    <!--// Stylesheets //-->
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/ddsmoothmenu.css" rel="stylesheet" type="text/css">
+    <link href="css/scrollbar.css" rel="stylesheet" type="text/css">
+    <!--// Javascript //-->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/ddsmoothmenu.js"></script>
+    <script type="text/javascript" src="js/menu.js"></script>
+    <script type="text/javascript" src="js/contentslider.js"></script>
+    <script type="text/javascript" src="js/jquery.1.4.2.js"></script>
+    <script type="text/javascript" src="js/jquery.lint.js"></script>
+    <script type="text/javascript" src="js/jquery.scroll.js"></script>
+    <script type="text/javascript" src="js/scroll.js"></script>
+    <script type="text/javascript" src="js/jquery.idTabs.min.js"></script>
+    <script type="text/javascript" src="js/switch.js"></script>
+    <script type="text/javascript" src="js/tabs.js"></script>
+    <script type="text/javascript" src="js/cufon-yui.js"></script><style type="text/css">cufon{text-indent:0!important;}@media screen,projection{cufon{display:inline!important;display:inline-block!important;position:relative!important;vertical-align:middle!important;font-size:1px!important;line-height:1px!important;}cufon cufontext{display:-moz-inline-box!important;display:inline-block!important;width:0!important;height:0!important;overflow:hidden!important;text-indent:-10000in!important;}cufon canvas{position:relative!important;}}@media print{cufon{padding:0!important;}cufon canvas{display:none!important;}}</style>
+    <script type="text/javascript" src="js/cufon.js"></script>
+    <script type="text/javascript" src="js/font.js"></script>
+    <!--[if lte IE 7]><style>.comments .commentlinks{margin-left:250px;}
+.comments .singlebtn{margin-left:380px;}
+.comments ul li:hover .commentlinks{display:inline-block;}
+.comments ul li.level2:hover .commentlinks{margin-left:210px;}
+.comments ul li.level2:hover .singlebtn{margin-left:340px;}
+</style><![endif]-->
+</head>
+
+<body class="bg2"><div class="ddshadow toplevelshadow" style="left: 615.271px; top: 89px; overflow: hidden; height: 0px; display: block;"></div>
+    <span class="smalllines">&nbsp;</span>
+    <!-- Wrapper -->
+    <div id="wrapper_sec">
+        <!-- Top Section -->
+        <div class="top_sec">
+            <!-- Top Section Left Links -->
+            <div class="toplinks">
+                <ul>
+                    <li class="first"><a href="index.html">Home</a></li>
+                    <li><a href="login.html">Login</a></li>
+                    <li><a href="signup.html">Signup</a></li>
+                    <li><a href="account.html">My account</a></li>
+                </ul>
+            </div>
+            <!-- Top Section right Links -->
+            <div class="links_icons">
+                <ul>
+                    <li class="last lang">Language: <a href="#"><img src="images/flag1.gif" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <!-- Header -->
+        <div id="masthead">
+            <!-- Logo -->
+            <div class="logo">
+                <a href="index.html"><img src="images/logo.png" style="width:100%;"></a>
+            </div>
+            <!-- Navigation -->
+            <div class="navigation">
+                <div id="smoothmenu1" class="ddsmoothmenu">
+                    <ul>
+                        <li><a href="index.html" class="staticlinks">Home</a></li>
+                        <li><a href="videos.html" class="staticlinks">Videos</a></li>
+                        <li><a href="blogs.html">Blogs</a>
+                        </li>
+                        <li style="z-index: 100;"><a href="ranklist_videos.html" class="staticlinks" style="padding-right: 30px;">Rank lists<img src="images/down.gif" class="downarrowclass" style="border:0;"></a>
+                            <ul style="display: none; top: 30px; visibility: visible;">
+                                <li><a href="ranklist_videos.html">Rank list of videos</a></li>
+                                <li><a href="ranklist_blogs.html">Rank list of blogs</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div class="clear"></div>
+                </div>
+            </div>
+            <!-- Search -->
+            <div class="search">
+                <input type="text" value="Enter keyword to search" id="searchBox" name="s" onblur="if(this.value == '') { this.value = 'Enter keyword to search'; }" onfocus="if(this.value == 'Enter keyword to search') { this.value = ''; }" class="bar">
+                <a href="#" class="go">&nbsp;</a>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <!-- Content Section -->
+        <div id="content_sec">
+            <!-- Bread Crumb -->
+            <div id="crumb">
+               
+                <ul>
+                    <li><a href="profile.html">Profile</a></li>
+                    <li><a href="payment.html">VIP </a></li>
+                    <li><a href="history.html">History </a></li>
+                </ul>
+                
+            </div>
+            
+            <div id="widget-logininfo">
+      <div class="usrSetting-wrap clearfix">
+           <a href="javascript:;" class="fl userSetting-head">
+                <img width="80" height="80" src="images/male-130.png" alt="" data-logininfo-elem="iconImg">
+                <div class="userSetting-changehead" data-logininfo-elem="changeiconbtn" style="font-size: 20px">change the head picture</div>
+          </a>
+          
+              <div class="userSetting-item  userName" data-logininfo-elem="nicknameline" style="font-size: 20px">
+                  
+                  <label>Nickname</label>                                     
+   <input type="text" name="nickname" placeholder="nickname"  >
+                  <br/>                                            
+                                                                                                             
+              </div>
+                                         
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                                                             
+                             
+              <div class="userSetting-item" style="font-size: 20px">
+                 <label>ID</label>                                     
+   <input type="text" name="id" placeholder="id"  >
+                  <br/> 
+              </div>
+              
+                  <!-- 弹层 -->
+                  <div class="mod-pop-upload" data-logininfo-elem="barhint" style="top: 30px; left: 75px; display:none;">
+                      <div class="pop-upload_inner">
+                          <!-- 弹层箭头位置包括lt(左上), lb(左下), rt(右上), rb(右下) -->
+                          
+                        <!--整体输出部分_END-->
+                      </div>
+                     
+       <script>
+            var isnotIEBrowser = (typeof isNotFuckInternetExplorer_571 != "undefined") ? isNotFuckInternetExplorer_571() : true;
+            var tabUserSettings = document.getElementById('tabUserSettings');
+            if(tabUserSettings && isnotIEBrowser){
+                tabUserSettings.style.display='none';
+            }
+    </script>
+  </div>
+              
+              <div id="widget-otherinfo">
+    <div class="userSetting-setWrap">
+
+                         <div class="userSetting-setWrap-item odd" data-otherinfo-line="gender" style="font-size: 20px">
+           <label>Gender</label>                                     
+   <input type="text" name="gender" placeholder="gender"  >
+                  <br/> 
+                             </div>
+                         <div class="userSetting-setWrap-item even" data-otherinfo-line="blood" style="font-size: 20px">
+            <label>Blood</label>                                     
+   <input type="text" name="blood" placeholder="blood"  >
+                  <br/> 
+                             </div>
+                        <div class="userSetting-setWrap-item odd" data-otherinfo-line="birthday" style="font-size: 20px">
+            <label>Birthday</label>                                     
+   <input type="text" name="birthday" placeholder="birthday"  >
+                  <br/> 
+                            </div>
+        <div class="userSetting-setWrap-item even" data-otherinfo-line="location" style="font-size: 20px">
+          <label>Home address</label>                                     
+   <input type="text" name="home address" placeholder="home address"  >
+                  <br/> 
+            </div>
+        <div class="userSetting-setWrap-item even" data-otherinfo-line="qq" style="font-size: 20px">
+        <label>QQ</label>                                     
+   <input type="text" name="qq" placeholder="qq" >
+                  <br/>
+   </div>
+            <div class="userSetting-setWrap-item even" data-otherinfo-line="email" style="font-size: 20px">
+     <label>Email</label>                                     
+   <input type="text" name="email" placeholder="email">
+                  <br/>
+                </div>
+                  <div class="userSetting-setWrap-item even" data-otherinfo-line="phonenumber" style="font-size: 20px">
+                   <label>Phone number</label>                                     
+   <input type="text" name="phone number" placeholder="phone number">  
+                  <br/>
+                  </div>
+                 <div class="clear"></div>
+    <!-- Footer -->
+    <div class="clear"></div>
+    <div id="copyrights">
+        <div class="inner">
+            <p>Copyright © 2019 Group 2. All rights reserved.</p>
+
+        </div>
+    </div>
+
+
+
+</body></html>
+        
+               
+       
+          
+        
+   
+                    
+              
+              
