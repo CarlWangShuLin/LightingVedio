de898e99981afc0d6f2b4654ccd73d291349a0c3
CarlWangShuLin <44082356+CarlWangShuLin@users.noreply.github.com>
2019-04-21 18:52:31 +0800

    Carl

--- includes/admin_logincheck.php
+++ includes/admin_logincheck.php
@@ -5,7 +5,7 @@ session_start();
 
 //if the session variable is not set, redirect to login.php
 if (!isset($_SESSION['login_user'])) {
-    header('location: ../login.php');
+    header('location: ../log/login.php');
 }
 
 if($_SESSION['login_level'] >= $pagelevel) {
