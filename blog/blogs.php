defea7c825dd92fe0ff52f0c13a32af910b30dc5
fitch001 <43442723+fitch001@users.noreply.github.com>
2019-04-26 17:17:32 +0800

    fitch blog update
    blog update

--- blog/blogs.php
+++ blog/blogs.php
@@ -6,20 +6,26 @@ require_once('../includes/rank_list_header.php');
 <link rel="stylesheet" href="../modal/bootstrap.min.css" />
 <script src="../modal/jquery.min.js"></script>
 <script src="../modal/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="../js/confirm/example/example.css">
+<script src="../js/confirm/lib/sweet-alert.js"></script>
+<link rel="stylesheet" href="../js/confirm/lib/sweet-alert.css">
 <title>Blogs</title>
+
 </head>
 
 <div id="content_sec" style="position:relative;">
+    <div style='position:absolute;margin-top:-20px;z-index:9999'>
+        <button style='display:inline;float:left;' data-toggle="modal" data-target="#myModal">Write New Blog</button>
+    </div>
 
     <!-- Column 1 -->
     <div class="col1">
-        <div class="blog">
+        <div class="blog" style="position:relative;">
             <h2 class="heading">Blog List</h2>
-            <div class="container">
-                <button class="btn btn-primary" data-toggle="modal" data-target="#myModal">Write New Blog</button>
-                <!--  定义模态框触发器，此处为按钮触发  -->
 
-                <form method="post" action="addblog.php" class="form-horizontal" role="form" id="myForm" onsubmit="return ">
+            <div class="container"> 
+                 <form method="post" action="addblog.php" class="form-horizontal" role="form" id="myForm" onsubmit="return ">
                     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                         <!--  定义模态框，过渡效果为淡入，id为myModal,tabindex=-1可以禁用使用tab切换，aria-labelledby用于引用模态框的标题，aria-hidden=true保持模态框在触发前窗口不可见  -->
                         <div class="modal-dialog">
@@ -66,7 +72,54 @@ require_once('../includes/rank_list_header.php');
                 </form>
             </div>
 
-            <a style="font-size: 24px;color: blue;" href="deblog.php"><button>Choose Blog Delete</button></a>
+            <div class="container"> 
+                 <form method="post" action="upblog.php" class="form-horizontal" role="form" id="myForm1" onsubmit="return ">
+                    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <!--  定义模态框，过渡效果为淡入，id为myModal,tabindex=-1可以禁用使用tab切换，aria-labelledby用于引用模态框的标题，aria-hidden=true保持模态框在触发前窗口不可见  -->
+                        <div class="modal-dialog">
+                            <!--  显示模态框对话框模型（若不写下一个div则没有颜色）  -->
+                            <div class="modal-content">
+                                <!--  显示模态框白色背景，所有内容都写在这个div里面  -->
+
+                                <div class="btn-info modal-header">
+                                    <h4>Write New Blog</h4>
+                                    <!--  模态框标题  -->
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <!--  关闭按钮  -->
+                                    <h4>您好，欢迎进入模态框</h4>
+                                    <!--  标题内容  -->
+                                </div>
+
+                                <div class="modal-body">
+                                    <!--  模态框内容，我在此处添加一个表单 -->
+                                    <form class="form-horizontal" role="form">
+                                        <div class="form-group">
+                                            <label for="bg_title" class="col-sm-2 control-label">Title</label>
+                                            <div class="col-sm-9">
+                                                <textarea rows="2" cols="100" id="bg_title" class="form-control well" name="bg_title" placeholder="Please Input Blog Title"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="bg_contents" class="col-sm-2 control-label">Contents</label>
+                                            <div class="col-sm-9">
+                                                <textarea rows="5" cols="100" id="bg_contents" class="form-control well" name="bg_contents" placeholder="Please Input Blog Title"></textarea>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+                                <div class="modal-footer">
+                                    <!--  模态框底部样式，一般是提交或者确定按钮 -->
+                                    <button type="submit" name="submit" class="btn btn-info">确定</button>
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                                </div>
+
+                            </div><!-- /.modal-content -->
+                        </div>
+                    </div> <!-- /.modal -->
+                </form>
+            </div>
+
             <ul class="bloglisting">
                 <?php
                 $num_rec_per_page = 6;   // 每页显示数量
@@ -90,9 +143,25 @@ require_once('../includes/rank_list_header.php');
                     $row3 = mysqli_fetch_array($result3);
                     echo "<li>";
                     echo "<div class='desc'>";
+
                     echo "<h3>" . "<a href='' class='colr'>" . $row["bg_title"] . "</a>" . "</h3>";
-                    echo "<button style='margin-left: 100%;'><a class='btn btn-primary' href='upblog.php?bg_id=" . $row["bg_id"] .
-                        "'>update</a></button>";
+
+
+                    if ($_SESSION['login_user'] == $row3['ac_name']) {
+                        echo "<div style='display:inline;float:right;'>";
+                        //echo "<ul class='examples'>";
+                        //echo "<li class='warning cancel'>";
+                        $bgid = $row["bg_id"];
+                        echo "<a href='deblog.php?bg_id=" . $row["bg_id"] . "' onclick='return del();'><button>Delete</button></a>";
+                        //echo "</li>";
+                        //echo "</ul>";
+                        echo "</div>";
+                        echo "<div style='display:inline;float:right;'>";
+                        echo "<a href='upblog.php?bg_id=" . $row["bg_id"] .
+                            "'><button>Update</button></a>";
+                        echo "</div>";
+                    } else { }
+
                     echo "<p class='time'>" . $row['bg_date'] . "</p>";
                     echo "<p class='postedby'>Posted By:" . $row3['ac_name'] . "</p>";
                     echo "<div class='clear'></div>";
@@ -100,9 +169,7 @@ require_once('../includes/rank_list_header.php');
                     echo "</div>";
                     echo "</li>";
                 }
-                //<a href="#" class="colr readmore">Read More</a>
-                //        <a href="#" class="colr readmore">Collection</a>
-                //        <a href="#" class="colr readmore">Download</a>
+
                 ?>
 
             </ul>
@@ -142,13 +209,56 @@ require_once('../includes/rank_list_header.php');
     </div>
 </div>
 
+
+
+
+<script>
+    function del() {
+        if (confirm("确定要删除吗？")) {
+            setTimeout(function() {
+                swal(
+                    "Deleted!",
+                    "Your imaginary file has been deleted!",
+                    "success",
+                );
+            }, 100);
+            return true;
+        } else {
+            swal("Cancelled", "Your imaginary file is safe :)", "error")
+            return false;
+        }
+    }
+</script>
+<script>
+    document.querySelector('ul.examples li.warning.cancel button').onclick = function() {
+        swal({
+                title: "Are you sure?",
+                text: "You will not be able to recover this imaginary file!",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: '#DD6B55',
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: "No, cancel plx!",
+                closeOnConfirm: false,
+                closeOnCancel: false
+            },
+            function(isConfirm) {
+                if (isConfirm) {
+                    swal("Deleted!", "Your imaginary file has been deleted!", "success");
+                } else {
+                    swal("Cancelled", "Your imaginary file is safe :)", "error");
+                }
+            });
+    };
+</script>
+
+
 <div class="clear"></div>
 <!-- Footer -->
 <div class="clear"></div>
 <div id="copyrights">
     <div class="inner">
         <p>Copyright © 2019 Group 2. All rights reserved.</p>
-
     </div>
 </div>
 
--- blog/deblog.php
+++ blog/deblog.php
@@ -2,44 +2,16 @@
 require_once('../includes/db.php');
 //require_once('../includes/head.php');
 
-if (isset($_POST['submit'])) {
-    $title = $_POST['select1'];
+$id = $_GET['bg_id'];
 
-    $query = "SELECT bg_id FROM blogs WHERE bg_title = '$title' ";
-    $result = mysqli_query($connection, $query);
-    $row = mysqli_fetch_array($result);
+// 2. Do a query (update information)
+$query = "DELETE FROM blogs ";
+$query .= "WHERE bg_id = $id";
 
-    $bg_id = $row['bg_id'];
-    $query = "delete from blogs ";
-    $query .= "WHERE bg_id = $bg_id ";
-    //echo $query;
-    header('Location: blogs.php');
-    mysqli_query($connection, $query);
-}
-
-$query2 = "SELECT * from blogs ";
-$result2 = mysqli_query($connection, $query2);
+//echo $query;
+header('Location: blogs.php');
+mysqli_query($connection, $query);
 
 
-?>
-
-
-<form role="form" action="deblog.php" method="post" style="backgroud-color: white;">
-
-    <div class="col-lg-6" style="margin-left:25%;">
-        <div class="form-group has-success">
-            <label>Select your blog to delete</label>
-            <select name="select1" class="form-control">
-                <option selected="selected">
-                    <?php echo "<--select-->"; ?>
-                </option>
-                <?php
-                while ($row2 = mysqli_fetch_array($result2)) {
-                    echo "<option value='$row2[bg_title]'>$row2[bg_title]</option>";
-                }
-                ?>
-            </select>
-        </div>
-        <button name="submit" class="btn">Select To Delete</button>
-    </div>
-</form> 
+// 5. close db connection
+mysqli_close($connection);
