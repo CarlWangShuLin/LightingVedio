e0a02b58ff36e9544c7d54a6b1d5c4f805fc5356
CarlWangShuLin <44082356+CarlWangShuLin@users.noreply.github.com>
2019-05-01 12:15:52 +0800

    change

--- admin/addan.php
+++ admin/addan.php
@@ -5,11 +5,10 @@ if (isset($_POST['submit'])) {
     $an_id = $_POST['an_id'];
     $an_name = $_POST['an_name'];
     $an_contents = $_POST['an_contents'];
-    $ac_id = $_POST['ac_id'];
 
 
-    $query  = "INSERT INTO announcements (an_id, an_name, an_contents, ac_id) ";
-    $query .= "VALUES ('$an_id', '$an_name', '$an_contents', '$ac_id') ";
+    $query  = "INSERT INTO announcements (an_id, an_name, an_contents) ";
+    $query .= "VALUES ('$an_id', '$an_name', '$an_contents') ";
 
     //echo $query;
     header('Location: admin.php?page=an_co');
--- admin/updatean.php
+++ admin/updatean.php
@@ -5,10 +5,9 @@ if (isset($_POST[submit])) {
     $an_id = $_POST['an_id'];
     $an_name = $_POST['an_name'];
     $an_contents = $_POST['an_contents'];
-    $ac_id = $_POST['ac_id'];
     
 // 2. Do a query (Select all medicinal)
-$query = "UPDATE announcements SET an_id='$an_id', an_name='$an_name', an_contents='$an_contents', ac_id='$ac_id' ";
+$query = "UPDATE announcements SET an_id='$an_id', an_name='$an_name', an_contents='$an_contents' ";
 $query .= "WHERE an_id= '$an_id'";
 
 //echo $query;
