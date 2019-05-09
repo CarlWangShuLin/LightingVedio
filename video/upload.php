2406f636edaf6adaf465b16f22dd1bb6f3494354
FantasyJay <42663790+FantasyJay@users.noreply.github.com>
2019-05-09 15:16:44 +0800

    update video functions

--- video/upload.php
+++ video/upload.php
@@ -5,6 +5,8 @@ require_once('../includes/db.php');
 $save_path = "../mp4/";                               //文件保存路径
 $max_size = 100000000000000000000000000000000000000000;  //上传文件最大值
 $allow_type = array('mp4');          //允许上传的类型
+$videoname = $_POST['videoname'];
+
 
 //判断保存的目录是否存在，如果不存在则创建保存目录
 if(!is_dir($save_path))
@@ -51,9 +53,9 @@ else{
 // 提交路径到数据库
 if (isset($_POST['submit'])) {
 
-    $query  = "INSERT INTO file (myfile) ";
-    $query .= "VALUES ('{$save_path}{$save_file_name}') ";
-        echo $query;
+    $query  = "INSERT INTO file (myfile, name) ";
+    $query .= "VALUES ('{$save_path}{$save_file_name}', '$videoname') ";
+    echo $query;
     mysqli_query($connection, $query);
 }
 ?>
